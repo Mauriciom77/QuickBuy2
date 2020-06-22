85610313682c8652c93e49714fc4bc88afa19725
mmenandro <mau77.mm@gmail.com>
2020-06-22 18:00:28 -0300

    Aula 91 final de login con redirecionamento para a pagina correta.

--- QuickBuy.Web/ClientApp/src/app/usuaria/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuaria/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Usuario } from "../../modelo/usuario";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-login",
@@ -7,23 +8,29 @@ import { Usuario } from "../../modelo/usuario";
   styleUrls: ["./login.component.css"]
 
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   public usuario;
-  
+  public returnUrl: string;
 
-  constructor() {
-    this.usuario = new Usuario();
+  constructor(private router: Router, private activatedRouter: ActivatedRoute) {
+    
   }
 
-
+    ngOnInit(): void {
+      this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
+      this.usuario = new Usuario();
+    }
 
   public email = "";
   public senha = "";
 
+  
+
   entrar() {
     if (this.usuario.email == "mau@teste.com" && this.usuario.senha == "abc123") {
-   
+      sessionStorage.setItem("usuario-autenticado", "1");
+      this.router.navigate([this.returnUrl]);
     }
     
   }
